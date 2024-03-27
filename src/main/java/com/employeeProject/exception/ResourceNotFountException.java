a8e723577904e5677386eb0a8c57e2c2459427ce
kalemangal11 <kalemangal1992@gmail.com>
2024-03-27 10:22:46 +0530

    we change fieldName to fldName

--- src/main/java/com/employeeProject/exception/ResourceNotFountException.java
+++ src/main/java/com/employeeProject/exception/ResourceNotFountException.java
@@ -8,13 +8,13 @@ import lombok.Setter;
 public class ResourceNotFountException extends RuntimeException {
 
 	String resourceName;
-	String fieldName;
+	String fldName;
 	Integer fieldValue;
 	
-	public ResourceNotFountException(String resourceName,String fieldName,Integer fieldValue) {
-		super(String.format("%s not found with %s:%s", resourceName, fieldName,fieldValue));
+	public ResourceNotFountException(String resourceName,String fldName,Integer fieldValue) {
+		super(String.format("%s not found with %s:%s", resourceName, fldName,fieldValue));
 		this.resourceName=resourceName;
-		this.fieldName=fieldName;
+		this.fldName=fldName;
 		this.fieldValue=fieldValue;
 	}
 }
